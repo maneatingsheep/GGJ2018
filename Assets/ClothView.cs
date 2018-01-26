6f9b5a663cbe8bd81b903f6b40397bdc8348be2d
Yoav <yoav@artlist.io>
2018-01-26 16:26:32 +0200

    first commit

---
+++ Assets/ClothView.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothView : MonoBehaviour
+{
+
+    public float closetSize = 200f;
+    public float _clothMovementDistance = 30f;
+    public GameObject clothPrefab;
+
+    private bool _isInitilized = false;
+    private int _amountOfCloths = 0;
+    private float[] clothsOriginalLocalPosition;
+    public GameObject[] cloths;
+
+
+    public void Awake()
+    {
+        Init(6);
+    }
+
+    public void Init(int amountOfCloths)
+    {
+        _isInitilized = true;
+        _amountOfCloths = amountOfCloths;
+
+        clothsOriginalLocalPosition = new float[amountOfCloths];
+        cloths = new GameObject[amountOfCloths];
+
+        for (var i = 0; i < amountOfCloths; i++)
+        {
+            cloths[i] = Instantiate(clothPrefab).gameObject;
+            cloths[i].transform.parent = gameObject.transform;
+            var distanceBetweenCloths = closetSize / amountOfCloths;
+            cloths[i].transform.localPosition = new Vector3(i * distanceBetweenCloths, 0);
+            clothsOriginalLocalPosition[i] = cloths[i].transform.localPosition.x;
+        }
+    }
+
+    public void SetAllClothsPosition(int index)
+    {
+        SetAllClothsPosition((float)index / _amountOfCloths);
+    }
+
+    public void SetAllClothsPosition(float ratio)
+    {
+
+        var index = (int)Mathf.Floor(ratio * _amountOfCloths);
+        var inIndexRatio = (ratio - index * (1f / _amountOfCloths)) / (1f / _amountOfCloths);
+
+        for (var i = 0; i < _amountOfCloths; i++)
+        {
+            if (i == index)
+            {
+                cloths[i].transform.localPosition = new Vector3(clothsOriginalLocalPosition[i] - inIndexRatio * _clothMovementDistance, 0);
+                continue;
+            }
+            if (i < index)
+            {
+                cloths[i].transform.localPosition = new Vector3(clothsOriginalLocalPosition[i] - 1 * _clothMovementDistance, 0);
+                continue;
+            }
+            cloths[i].transform.localPosition = new Vector3(clothsOriginalLocalPosition[i], 0);
+        }
+    }
+
+    public void OnRatioChanged(float ratio)
+    {
+        if (!_isInitilized)
+        {
+            return;
+        }
+        SetAllClothsPosition(ratio);
+    }
+
+}
---
+++ Assets/ScrollHandler.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollHandler : MonoBehaviour
+{
+
+    public GameObject ScrollIndecator;
+    public RectTransform ThisRectTransform;
+    public ClothView ClothView;
+
+    private float _xMinLimit;
+    private float _xMaxLimit;
+    private float _totalDis;
+
+    private int _amountOfCloths = 0;
+    private float _clothIndexSize;
+
+    private bool _isInitilized = false;
+
+    public void Awake()
+    {
+        _xMaxLimit = ThisRectTransform.rect.width/2;
+        _xMinLimit = -_xMaxLimit;
+        _totalDis = _xMaxLimit - _xMinLimit;
+        Init(6);
+    }
+
+    public void Start()
+    {
+        SetAllClothsPosition(2);
+    }
+    
+    public void Init(int amountOfCloths)
+    {
+        _amountOfCloths = amountOfCloths;
+        _clothIndexSize = _totalDis / _amountOfCloths;
+        _isInitilized = true;
+    }
+
+
+    public void SetAllClothsPosition(int index)
+    {
+        var ratio = (float)index / _amountOfCloths;
+
+        var indecatorTran = ScrollIndecator.transform;
+        indecatorTran.localPosition = new Vector3(_xMinLimit + ratio * _totalDis, indecatorTran.localPosition.y);
+
+        ClothView.OnRatioChanged(ratio);
+    }
+
+
+    public void OnValueChanged(Vector2 position)
+    {
+        var indecatorTran = ScrollIndecator.transform;
+        if (indecatorTran.localPosition.x > _xMaxLimit)
+        {
+            indecatorTran.localPosition = new Vector3(_xMaxLimit, indecatorTran.localPosition.y);
+        }
+        if (indecatorTran.localPosition.x < _xMinLimit)
+        {
+            indecatorTran.localPosition = new Vector3(_xMinLimit, indecatorTran.localPosition.y);
+        }
+
+        if (!_isInitilized)
+        {
+            return;
+        }
+        
+        var ratio = 1 - (_xMaxLimit - indecatorTran.localPosition.x) / _totalDis;
+
+        ClothView.OnRatioChanged(ratio);
+    }
+
+}
