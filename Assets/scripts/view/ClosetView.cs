1ef5f3b3067e4c44d4e5bddeb3ad9bf23b5a80dc
Yoav <yoav@artlist.io>
2018-01-27 16:30:04 +0200

    closet

--- Assets/scripts/view/ClosetView.cs
+++ Assets/scripts/view/ClosetView.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClosetView : MonoBehaviour {
 
     public GameplayMaster GameplayMasterRef;
 
+    private int _clothCategory = 0;
+    public Button[] Buttons;
+
     public void Init() {
-        
+        Buttons[_clothCategory].Select();
     }
 
-    public void IncDress(int cat) {
-        GameplayMasterRef.SetDress(true, cat);
-        
+    public void IncDress() {
+        GameplayMasterRef.SetDress(true, _clothCategory);
+        Buttons[_clothCategory].Select();
     }
 
-    public void DecDress(int cat) {
-        GameplayMasterRef.SetDress(false, cat);
+    public void DecDress() {
+        GameplayMasterRef.SetDress(false, _clothCategory);
+        Buttons[_clothCategory].Select();
     }
+
+    public void OnClothCategoryChange(int clothCategory)
+    {
+        _clothCategory = clothCategory;
+    }
+
+
 }
